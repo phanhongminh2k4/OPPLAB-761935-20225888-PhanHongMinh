f7f6a411289a4c48f397d15a655418861891c8d3
phanhongminh2k4 <hongminha321dqh@gmail.com>
2026-03-29 00:41:37 +0700

    InputFromKeyboard

--- Lab01/6.2.java
+++ Lab01/6.2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class InputFromKeyboard {
+public class InputFromKeyboard { 
     public static void main(String args[]) {
         Scanner keyboard = new Scanner(System.in);
 
